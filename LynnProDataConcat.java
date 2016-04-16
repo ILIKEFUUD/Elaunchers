c9820c54593028f6f5c7be814df30373059ac4ce
ILIKEFUUD <rahulpshah@live.com>
2016-04-16 16:07:59 -0400

    Added LynnProDataConcat to GitHub

---
+++ LynnProDataConcat.java
@@ -0,0 +1,23 @@
+/*
+ * Rahul Shah
+ * 4/16/2016
+ * 
+ * V 1.0 
+ * 
+ * Merges 2 Excel files into a text file 
+ * Separated by quotes and commas
+ * 
+ * Format: Full name, company, transaction 1, transaction 2, ..., total spent, email, fax, phone
+ */
+public class LynnProDataConcat {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//import files
+		
+		
+
+	}
+
+}
