9d1dbf71a2edf65d74c3687f60fdd2b660de94a1
ILIKEFUUD <rahulpshah@live.com>
2016-04-16 18:43:35 -0400

    Removed bad code

--- DataElement.java
+++ DataElement.java
@@ -23,27 +23,11 @@ public class DataElement {
 		// info = new String[7];//holds 8 information values for each company
 
 		transactions = new ArrayList<Integer>();
-		
+
 		data = new ArrayList<String>();
 
 		// first name, last name, email, title, department, phone, fax
 	}
-
-	/*
-	 * public void addFName(String Fname) { info[0] = Fname; }
-	 * 
-	 * public void addLName(String Lname) { info[1] = Lname; }
-	 * 
-	 * public void addEmail(String email) { info[2] = email; }
-	 * 
-	 * public void addTitle(String title) { info[3] = title; }
-	 * 
-	 * public void addDept(String dept) { info[4] = dept; }
-	 * 
-	 * public void addPhone(String phone) { info[5] = phone; }
-	 * 
-	 * public void addFax(String fax) { info[6] = fax; isComplete = true; }
-	 */
 
 	public void add(String dataEntry) {
 		data.add(dataEntry);
@@ -54,23 +38,10 @@ public class DataElement {
 	}
 
 	// override toString
-	//temporary, TODO: CHANGE TO BE CORRECT FORMATTING FOR CSV
-	public String toString(){
-		return name + data.toString() + transactions.toString();
+	// temporary, TODO: CHANGE TO BE CORRECT FORMATTING FOR CSV
+	public String toString() {
+		return "\"" + name + "\"," + "\"," + data.toString() + "\"," + "\"," + transactions.toString();
 	}
-	
-
-	/*
-	 * public String toString(){ String print = "\"" + name + "\",";
-	 * 
-	 * for (String i : info) print+= "\"" + i + "\",";
-	 * 
-	 * for (int m : transactions) print+= "\"" + m + "\",";
-	 * 
-	 * return print;
-	 * 
-	 * }
-	 */
 
 	public boolean done() {
 		return data.size() == 7;
